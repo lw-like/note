539d20dbc88f3d99b0c6905bedf725576a7421ff
lw-like <wrobelllukasz@gmail.com>
2025-05-08 22:52:42 +0200

    refactor(): Ref management

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+mod controllers;
 mod model;
 mod services;
 mod structs;
-mod controllers;
 
 use controllers::note_controller::NoteController;
 use services::cmd_service::{self as cmd, get_args_cmd};
@@ -9,12 +9,12 @@ use services::message_service::MessagesService;
 use structs::input_file::Readable;
 use structs::input_std::InputStd;
 
-fn handle_args(note_ctrl: &NoteController, value: &String) -> bool {
+fn handle_args(note_ctrl: &NoteController, value: String) -> bool {
     let cmd_type = cmd::get_cmd_type(&value);
 
     match cmd_type {
         cmd::CmdType::List => note_ctrl.print_current_notes(),
-        cmd::CmdType::Note => note_ctrl.save_daily_note(value),
+        cmd::CmdType::Note => note_ctrl.save_daily_note(&value),
         cmd::CmdType::SpawnRows => note_ctrl.spawn_sample_rows(),
         cmd::CmdType::Interactive => false,
     }
@@ -22,18 +22,15 @@ fn handle_args(note_ctrl: &NoteController, value: &String) -> bool {
 
 fn handle_user_input(note_ctrl: &NoteController) {
     let inp = InputStd;
-    let note_value = match inp.read::<String>() {
-        Some(val) => val,
-        None => String::new()
-    };
+    let note_value = inp.read::<String>().unwrap_or_default();
 
-    handle_args(&note_ctrl, &note_value);
+    handle_args(note_ctrl, note_value);
 }
 
 fn main() {
     let note_ctrl = NoteController::new();
 
-    if handle_args(&note_ctrl, &get_args_cmd()) {
+    if handle_args(&note_ctrl, get_args_cmd()) {
         return;
     }
 
