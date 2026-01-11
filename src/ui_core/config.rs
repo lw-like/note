5389fab3cb96b9c6dad7fdffe56406b7106dc79e
Like <wrobelllukasz@gmail.com>
2026-01-11 07:48:29 +0100

    chore: removed unused code

--- src/ui_core/config.rs
+++ src/ui_core/config.rs
@@ -1,10 +1,6 @@
-// #![allow(dead_code)] // Remove this once you start using the code
-
-use std::{collections::HashMap, env, path::PathBuf};
+use std::{collections::HashMap, path::PathBuf};
 
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
-use directories::ProjectDirs;
-use lazy_static::lazy_static;
 use ratatui::style::{Color, Modifier, Style};
 use serde::{de::Deserializer, Deserialize};
 use tracing::error;
